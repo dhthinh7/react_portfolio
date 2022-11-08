46119b1b589b82235d9ad604ca3a7ce934104891
dhthinh7 <dhthinh7@gmail.com>
2022-11-09 01:26:18 +0700

    Fix layout for header
    Add live demo
    
    Fix layout for header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -7,14 +7,16 @@ export default function Header() {
   return <>
     <header>
       <div className="container header_container">
-        <h5>Hello I'm</h5>
-        <h2>Thinh Doan</h2>
-        <h5 className="text-light">Fontend developer</h5>
-        <CV />
-        <HeaderSocials />
+        <div className="header_infor">
+          <h5>Hello I'm</h5>
+          <h2>Thinh Doan</h2>
+          <h5 className="text-light">Fontend developer</h5>
+          <CV />
+        </div>
         <div className="avatar">
           <img src={avatar} alt="avatar" />
         </div>
+        <HeaderSocials />
         <a href="#contact" className="scroll_down">Scroll down</a>
       </div>
     </header>
